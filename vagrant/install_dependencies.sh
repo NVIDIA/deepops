9f53092e01a156db8cd21b235673318d6e4dc137
Michael Balint <mbalint@nvidia.com>
2018-10-30 12:59:01 -0400

    Removed virtualbox install, swapped with libvirt.

--- vagrant/install_dependencies.sh
+++ vagrant/install_dependencies.sh
@@ -5,8 +5,12 @@ set -xe
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt update
 sudo apt install -y ansible
-sudo apt install -y virtualbox virtualbox-ext-pack vagrant
+sudo apt install -y vagrant
 sudo pip install netaddr
+
+# install kvm packages
+sudo apt install -y qemu-kvm libvirt-bin bridge-utils libguestfs-tools
+sudo apt install -y qemu ovmf virt-manager firewalld
 
 # install vagrant plugins
 vagrant plugin install vagrant-hostmanager vagrant-libvirt
