ecd80f5ce537804b5e1751a192e608a832965817
Adam Tetelman <supertetelman@gmail.com>
2019-01-28 22:55:26 +0000

    Fix rook pod name bug in polling script

--- scripts/ceph_poll.sh
+++ scripts/ceph_poll.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+rook_tools_pod=$(kubectl -n rook-ceph get pod -l app=rook-ceph-tools -o name | cut -d \/ -f2 | sed -e 's/\\r$//g')
+
 while true; do
-    kubectl -n rook-ceph exec -ti rook-ceph-tools ceph status # Run once to print output
-    kubectl -n rook-ceph exec -ti rook-ceph-tools ceph status | grep "mds: cephfs" | grep "up:active" | grep "standby-replay" # Run again to check for completion
+    kubectl -n rook-ceph exec -ti $rook_tools_pod ceph status # Run once to print output
+    kubectl -n rook-ceph exec -ti $rook_tools_pod ceph status | grep "mds: cephfs" | grep "up:active" | grep "standby-replay" # Run again to check for completion
     if [ "${?}" == "0" ]; then
     	echo "Ceph has completed setup."
         break
