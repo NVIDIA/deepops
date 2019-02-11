0e40a302b4953027b127a765b1af43d8352c985c
Luke Yeager <lukeyeager@users.noreply.github.com>
2019-02-10 20:13:30 -0800

    Updates to the vagrant workflow (#50)
    * Virtual: install netaddr with system pkg
    
    * Virtual: install working version of vagrant
    
    See dotless-de/vagrant-vbguest#292
    See https://bugs.launchpad.net/ubuntu/+source/vagrant/+bug/1796168
    
    * Virtual: install libvirt-dev
    
    Otherwise, you get errors compiling some gems while installing vagrant
    plugins
    
    * Virtual: become root to use vagrant
    
    * Virtual: actually, don't do sudo+vagrant
    
    * Virtual: install sshpass

--- virtual/cluster_up.sh
+++ virtual/cluster_up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -ex
 
 # start vagrant
 vagrant up --no-parallel --provider=libvirt
--- virtual/install_dependencies.sh
+++ virtual/install_dependencies.sh
@@ -5,11 +5,22 @@ set -xe
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt update
 sudo apt install -y ansible
-sudo apt install -y vagrant
-sudo pip install netaddr
+
+if [ "$(lsb_release -cs)" = "bionic" ]; then
+    # dotless-de/vagrant-vbguest#292
+    pushd "$(mktemp -d)"
+    wget https://releases.hashicorp.com/vagrant/2.0.3/vagrant_2.0.3_x86_64.deb -O vagrant.deb
+    sudo dpkg -i vagrant.deb
+    popd
+else
+    sudo apt install -y vagrant
+fi
+
+sudo apt install -y python-netaddr
+sudo apt install -y sshpass
 
 # install kvm packages
-sudo apt install -y qemu-kvm libvirt-bin bridge-utils libguestfs-tools
+sudo apt install -y qemu-kvm libvirt-bin libvirt-dev bridge-utils libguestfs-tools
 sudo apt install -y qemu ovmf virt-manager firewalld
 
 # install vagrant plugins
