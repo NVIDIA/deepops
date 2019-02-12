aafa430bfae55b8f73e08121a3985a2b01e54d26
Douglas Holt <dholt@nvidia.com>
2019-02-12 20:31:33 +0000

    begin code cleanup and refactor

--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -1,10 +1,42 @@
 #!/usr/bin/env bash
 
-type ansible >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-    sudo apt-add-repository -y ppa:ansible/ansible
-    sudo apt-get update
-    sudo apt-get -y install ansible
-    ansible --version
-fi
+. /etc/os-release
 
+case "$ID_LIKE" in
+    rhel*)
+        type ansible >/dev/null 2>&1
+        if [ $? -ne 0 ] ; then
+            sudo yum -y install ansible
+        fi
+        ansible --version
+
+        python -c 'import netaddr' >/dev/null 2>&1
+        if [ $? -ne 0 ] ; then
+            sudo yum -y install ansible python-netaddr
+        fi
+        ;;
+    debian*)
+        type apt-add-repository >/dev/null 2>&1
+        if [ $? -ne 0 ] ; then
+            sudo apt-get update
+            sudo apt-get -y install software-properties-common
+        fi
+
+        type ansible >/dev/null 2>&1
+        if [ $? -ne 0 ] ; then
+            sudo apt-add-repository -y ppa:ansible/ansible
+            sudo apt-get update
+            sudo apt-get -y install ansible
+        fi
+        ansible --version
+
+        python -c 'import netaddr' >/dev/null 2>&1
+        if [ $? -ne 0 ] ; then
+            sudo apt-get -y install python-netaddr
+        fi
+        ;;
+    *)
+        echo "Unsupported Operating System $ID_LIKE"
+        exit 1
+        ;;
+esac
