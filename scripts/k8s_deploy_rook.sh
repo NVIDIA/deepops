4f927e52aadb45546af9f84dfdae741e06af3693
dholt <dholt@nvidia.com>
2019-02-19 08:50:41 -0800

    Squashed commit of the following:
    commit 4dc25e8990578a7475f41422025571c48dba44c0
    Merge: 27303c6 6857cbc
    Author: dholt <dholt@nvidia.com>
    Date:   Tue Feb 19 08:50:09 2019 -0800
    
        Merge branch 'refactor' into k8s-addons
    
    commit 27303c604e3a783f7c2f681a90df7d30c77d2175
    Merge: 5d30774 2b377fc
    Author: dholt <dholt@nvidia.com>
    Date:   Tue Feb 19 08:49:19 2019 -0800
    
        Merge branch 'k8s-addons' of github.com:NVIDIA/deepops into k8s-addons
    
    commit 5d30774a291eec7efe94f59e76ff75dee0956db9
    Author: dholt <dholt@nvidia.com>
    Date:   Tue Feb 19 08:49:11 2019 -0800
    
        be a little more heavy-handed removing swap than kubespray
    
    commit 2b377fc0cfd182a14defb9bd0e03bbe48dc75d55
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 22:28:25 2019 +0000
    
        fix typo
    
    commit 7337dbda532636730a39b7cda0722522e1bd8c86
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 22:27:44 2019 +0000
    
        add some notes
    
    commit ceca6af73c069d250cb54ffbe3fc4196482db02d
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 22:20:59 2019 +0000
    
        fix redirect
    
    commit 4fd009773b6c1d722b985c99dbef2a0596fd4180
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 22:18:54 2019 +0000
    
        require storage
    
    commit 7f3d9c7e357a4953b6fd913e6c0b95d75a421fdd
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 21:46:32 2019 +0000
    
        add rook install script, rename kubeflow script
    
    commit 844a1b374bcb0e279761bc9793ba0b31b1034ffe
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 21:31:17 2019 +0000
    
        mods
    
    commit c5fd018e61f9ef96511267c19e1f875eec6abfc6
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 21:24:27 2019 +0000
    
        mods
    
    commit d969ce0e58d054d528e379a17684c9ed28acca15
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 19:47:08 2019 +0000
    
        mods
    
    commit babf6c47167940433f41d59548ca61d256b568de
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 19:17:06 2019 +0000
    
        mods
    
    commit 88ffc1332e54c45d8152a98601c08e608ec532f9
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 19:08:11 2019 +0000
    
        move and add kubeflow
    
    commit 6252e6cdf39c2811735aeb84097b97b0d4608b4e
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 18:59:27 2019 +0000
    
        improve ks install script
    
    commit 62658c7e7113061b7abc6999b11a9305fc17b0c7
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 18:57:10 2019 +0000
    
        improve ks install script
    
    commit 90300c85ac3e4cae283ca0730a330a4a1217883a
    Author: Douglas Holt <dholt@nvidia.com>
    Date:   Mon Feb 18 18:53:45 2019 +0000
    
        initial ks install script

---
+++ scripts/k8s_deploy_rook.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+type helm >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+    ./scripts/install_helm.sh
+fi
+
+helm repo list | grep rook-master >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+    helm repo add rook-master https://charts.rook.io/master
+fi
+
+helm status rook-ceph >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+    helm install --namespace rook-ceph-system --name rook-ceph rook-master/rook-ceph --version v0.9.0-79.g1a1ffdd
+fi
+
+kubectl -n rook-ceph get pod -l app=rook-ceph-tools 2>&1 | grep "No resources found." >/dev/null 2>&1
+if [ $? -eq 0 ] ; then
+    kubectl create -f services/rook-cluster.yml
+fi
+
+sleep 5
+
+# Get Rook Ceph Tools POD name
+toolspod=$(kubectl -n rook-ceph get pod -l app=rook-ceph-tools --no-headers -o custom-columns=:.metadata.name)
+
+echo
+echo "Ceph deployed, it may take up to 10 minutes for storage to be ready"
+echo "Monitor readiness with:"
+echo "kubectl -n rook-ceph exec -ti ${toolspod} ceph status | grep up:active"
+echo
