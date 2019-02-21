f5e57f97f61ff9a09ec17f848895966599219f17
Michael Balint <mbalint@nvidia.com>
2019-02-20 19:42:52 -0500

    Refactored references to vagrant --> virtual.

--- virtual/cluster_up.sh
+++ virtual/cluster_up.sh
@@ -13,14 +13,14 @@ ansible management -u root -e "ansible_user=root ansible_password=deepops" -b -a
 ansible-playbook -l management -v -b --flush-cache -e "@config/kube.yml" -e "ansible_user=root ansible_password=deepops" kubespray/cluster.yml
 
 # Set up Kubernetes for remote administration
-KUBECONFIG='./vagrant/admin.conf'
-ansible management -u root -e "ansible_user=root ansible_password=deepops" -b -m fetch -a "src=/etc/kubernetes/admin.conf flat=yes dest=./vagrant/"
+KUBECONFIG='./virtual/admin.conf'
+ansible management -u root -e "ansible_user=root ansible_password=deepops" -b -m fetch -a "src=/etc/kubernetes/admin.conf flat=yes dest=./virtual/"
 sed -i '/6443/c\    server: https:\/\/mgmt01:6443' ${KUBECONFIG}
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl && mv ./kubectl ./vagrant/kubectl
+chmod +x ./kubectl && mv ./kubectl ./virtual/kubectl
 
 # Launch the apt repo service on k8s
-KUBECONFIG=$KUBECONFIG ./vagrant/kubectl apply -f services/apt.yml
+KUBECONFIG=$KUBECONFIG ./virtual/kubectl apply -f services/apt.yml
 
 # Set up software
 ansible-playbook -e "ansible_user=root ansible_password=deepops" -l login,dgx-servers --skip-tags skip-for-virtual ansible/playbooks/software.yml
