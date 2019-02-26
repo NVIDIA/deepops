6ef90ba694a94fcf05697ebe32cd107fb9b15b5e
Michael Balint <mbalint@nvidia.com>
2019-02-25 20:55:38 -0500

    Using gpu-servers and gpu01 naming convention now. Moved config into virtual local directory.

--- virtual/cluster_up.sh
+++ virtual/cluster_up.sh
@@ -10,22 +10,22 @@ virsh list
 cd ..
 
 # Create the config for deepops servers (and use the virtual inventory)
-cp -r config.example/ config/
-cp virtual/virtual_inventory config/inventory
+cp -r config.example/ virtual/config/
+cp virtual/virtual_inventory virtual/config/inventory
 # Make sure to use the `vagrant` user instead of `ubuntu`
 sed -i 's/ansible_user: ubuntu/ansible_user: vagrant/g' config/group_vars/all.yml 
 
 # Bootstrap the VMs so that they can be accessible with the rest of the ansible playbooks without
 # needing a password
-ansible-playbook -T 30 -e "ansible_user=vagrant ansible_password=vagrant" --skip-tags skip-for-virtual playbooks/bootstrap-ansible.yml
-ansible-playbook -T 30 -e "ansible_user=vagrant ansible_password=vagrant" --skip-tags skip-for-virtual playbooks/bootstrap.yml
+ansible-playbook -i virtual/config/inventory -T 30 -e "ansible_user=vagrant ansible_password=vagrant" --skip-tags skip-for-virtual playbooks/bootstrap-ansible.yml
+ansible-playbook -i virtual/config/inventory -T 30 -e "ansible_user=vagrant ansible_password=vagrant" --skip-tags skip-for-virtual playbooks/bootstrap.yml
 
 # Deploy Kubernetes on mgmt
-ansible-playbook -e "ansible_user=vagrant" -l management -v -b --flush-cache -e "@config/kube.yml" kubespray/cluster.yml
+ansible-playbook -i virtual/config/inventory -e "ansible_user=vagrant" -l management -v -b --flush-cache -e "@config/kube.yml" kubespray/cluster.yml
 
 # Set up Kubernetes for remote administration
 KUBECONFIG='./virtual/admin.conf'
-ansible management -e "ansible_user=vagrant" -b -m fetch -a "src=/etc/kubernetes/admin.conf flat=yes dest=./virtual/"
+ansible -i virtual/config/inventory management -e "ansible_user=vagrant" -b -m fetch -a "src=/etc/kubernetes/admin.conf flat=yes dest=./virtual/"
 sed -i '/6443/c\    server: https:\/\/mgmt:6443' ${KUBECONFIG}
 #cp ./virtual/admin.conf ~/.kube/config
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
@@ -57,36 +57,36 @@ sleep 15
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm install --values config/registry.yml stable/docker-registry --version 1.4.3
 
-# Install nvidia drivers on dgx-servers
-ansible-playbook -l dgx-servers -e "ansible_user=vagrant" playbooks/nvidia-driver.yml
+# Install nvidia drivers on gpu-servers
+ansible-playbook -i virtual/config/inventory -l gpu-servers -e "ansible_user=vagrant" playbooks/nvidia-driver.yml
 
-# Install nvidia-docker2 on dgx-servers
-ansible-playbook -l dgx-servers -e "ansible_user=vagrant" playbooks/nvidia-docker.yml
+# Install nvidia-docker2 on gpu-servers
+ansible-playbook -i virtual/config/inventory -l gpu-servers -e "ansible_user=vagrant" playbooks/nvidia-docker.yml
 
 # Set up the NVIDIA GPU device plugin for K8s
-ansible-playbook -e "ansible_user=vagrant" playbooks/k8s-gpu-plugin.yml
+ansible-playbook -i virtual/config/inventory -e "ansible_user=vagrant" playbooks/k8s-gpu-plugin.yml
 
-# Rerun kubespray to set up the dgx-servers and join them to the cluster
-ansible-playbook -l k8s-cluster -e "ansible_user=vagrant" -v -b --flush-cache --extra-vars "@config/kube.yml" kubespray/cluster.yml
+# Rerun kubespray to set up the gpu-servers and join them to the cluster
+ansible-playbook -i virtual/config/inventory -l k8s-cluster -e "ansible_user=vagrant" -v -b --flush-cache --extra-vars "@config/kube.yml" kubespray/cluster.yml
 
 # Show the nodes
 ./virtual/kubectl get nodes
 
 # Install the nvidia container runtime
-ansible-playbook -l k8s-gpu -e "ansible_user=vagrant" -v -b --flush-cache --extra-vars "@config/kube.yml" playbooks/k8s-gpu.yml
+ansible-playbook -i virtual/config/inventory -l k8s-gpu -e "ansible_user=vagrant" -v -b --flush-cache --extra-vars "@config/kube.yml" playbooks/k8s-gpu.yml
 
-# Show that dgx01 has GPU resources
-./virtual/kubectl describe node dgx01
+# Show that 01 has GPU resources
+./virtual/kubectl describe node gpu01
 
 # Deploy monitoring (prometheus + grafana stack)
 helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 helm install coreos/prometheus-operator --name prometheus-operator --namespace monitoring --values config/prometheus-operator.yml
 ./virtual/kubectl create configmap kube-prometheus-grafana-gpu --from-file=config/gpu-dashboard.json -n monitoring
 helm install coreos/kube-prometheus --name kube-prometheus --namespace monitoring --values config/kube-prometheus.yml
-# Label the gpu nodes (dgx-servers)
-./virtual/kubectl label nodes dgx01 hardware-type=NVIDIAGPU
+# Label the gpu nodes (gpu-servers)
+./virtual/kubectl label nodes gpu01 hardware-type=NVIDIAGPU
 ./virtual/kubectl create -f services/dcgm-exporter.yml
 
 # Deploy slurm
-ansible-playbook -e "ansible_user=vagrant" -l slurm-cluster playbooks/slurm.yml
+ansible-playbook -i virtual/config/inventory -e "ansible_user=vagrant" -l slurm-cluster playbooks/slurm.yml
 
