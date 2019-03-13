d21d0a2fd5bb6c9d8c055153e3516782bbaaaae9
Douglas Holt <dholt@nvidia.com>
2019-03-13 21:37:20 +0000

    set/reset GPU clocks

---
+++ roles/slurm/files/shared/bin/set_gpu_clocks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+gpu_count="$(nvidia-smi -L | wc -l)"
+
+case "$1" in
+    default)
+        nvidia-smi -rac           # Reset application clocks
+        nvidia-smi -acp 0         # Reset application clock permissions
+        nvidia-smi -c DEFAULT     # Reset compute mode to default
+        ;;
+    max)
+        for i in $(seq 0 "$(( gpu_count - 1 ))" ) ; do
+            next.sm="$(nvidia-smi -i "$i" --query-gpu=clocks.max.sm --format=csv,noheader,nounits)"
+            next.mem="$(nvidia-smi -i "$i" --query-gpu=clocks.max.mem --format=csv,noheader,nounits)"
+            nvidia-smi -i "${i}" -ac "${next.mem}","${next.sm}"
+        done
+        ;;
+    *)
+        echo "Usage: $0 [default|max]"
+        exit 1
+        ;;
+esac
