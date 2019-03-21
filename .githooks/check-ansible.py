efeb73ea6f6831d14bb137d4b09c6c3144584ca0
Adam DeConinck <adeconinck@nvidia.com>
2019-03-21 09:07:05 -0700

    check-ansible script lints changed ansible roles/playbooks

--- .githooks/check-ansible.py
+++ .githooks/check-ansible.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+"""
+Get a list of Ansible playbooks and roles that have changes staged in Git.
+Run ansible-lint on only those playbooks and roles.
+"""
+
 
 from __future__ import print_function
 import subprocess
 import re
+
 
 def get_changed_ansible_paths():
     """
@@ -17,7 +23,18 @@ def get_changed_ansible_paths():
         # Add role directories
         role_match = re.match(r"^roles/(\w+)/.*", f)
         if role_match:
-            ansible_lint_paths_to_check.append(role_match.group(1))
+            ansible_lint_paths_to_check.append(
+                "roles/{}".format(role_match.group(1)))
     return ansible_lint_paths_to_check
 
-print(get_changed_ansible_paths())
+
+def run_ansible_lint(paths):
+    cmd = ["ansible-lint"] + paths
+    subprocess.call(cmd)
+
+
+if __name__ == "__main__":
+    paths = get_changed_ansible_paths()
+    print(paths)
+    if len(paths) > 0:
+        run_ansible_lint(paths)
