458ada0219aa935f342376060baa1b0081d83f99
Adam DeConinck <adeconinck@nvidia.com>
2019-03-25 12:27:08 -0700

    add check/retry logic when turning up ingress

--- scripts/k8s_deploy_ingress_metallb.sh
+++ scripts/k8s_deploy_ingress_metallb.sh
@@ -16,15 +16,32 @@ fi
 # Set up the MetalLB load balancer
 if ! helm status metallb >/dev/null 2>&1; then
 	helm install --values "${config_dir}/helm/metallb.yml" --name metallb stable/metallb
-	echo "Waiting for load balancer to come online"
-	sleep 60
 fi
 
+for i in {1..10}; do
+	echo "Check for load balancer online... check ${i}"
+	if kubectl get pods -l app=metallb,component=controller --no-headers | grep -i running;
+	then
+		break
+	else
+		sleep 30
+	fi
+done
 
 # Set up the ingress controller
 if ! helm status nginx-ingress >/dev/null 2>&1; then
 	helm install --name nginx-ingress --values "${config_dir}/helm/ingress.yml" stable/nginx-ingress
 fi
+
+for i in {1..10}; do
+	echo "Check for ingress online... check ${i}"
+	if kubectl get pods -l app=nginx-ingress,component=controller --no-headers | grep -i running;
+	then
+		break
+	else
+		sleep 30
+	fi
+done
 
 # Show services
 kubectl get services
