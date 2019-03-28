a0d2384992dd96a5b3c2815f2f208fc1c9ab6039
Adam DeConinck <adeconinck@nvidia.com>
2019-03-28 10:18:40 -0700

    script to clone fresh deepops repo with galaxy deps

---
+++ scripts/offline/fresh_deepops_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+DEEPOPS_REPO_URL="${DEEPOPS_REPO_URL:-https://github.com/NVIDIA/deepops}"
+REPO_DEST_DIR="${REPO_DEST_DIR:-/tmp/deepops/deepops}"
+
+if [ ! -d "${REPO_DEST_DIR}" ]; then
+	mkdir -p "${REPO_DEST_DIR}"
+fi
+
+git clone --recursive "${DEEPOPS_REPO_URL}" "${REPO_DEST_DIR}"
+
+(
+  cd "${REPO_DEST_DIR}" && \
+  ansible-galaxy install -p "${REPO_DEST_DIR}/ansible-galaxy" -r requirements.yml
+)
