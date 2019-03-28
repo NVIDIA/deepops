317037dea8655f26d705d3401f5d36b759a6bd02
Adam DeConinck <adeconinck@nvidia.com>
2019-03-28 13:30:29 -0700

    fixes to get right vars in during k8s playbook

--- scripts/k8s_inventory.sh
+++ scripts/k8s_inventory.sh
@@ -3,10 +3,14 @@
 git submodule update --init
 
 k8s_config_dir=${K8S_CONFIG_DIR:-./k8s-config}
+deepops_config=${DEEPOPS_CONFIG_DIR:-$(pwd)/config}
 
 if [ ! -d "${k8s_config_dir}" ] ; then
 	# Copy the kubespray default configuration
 	cp -rfp kubespray/inventory/sample/ "${k8s_config_dir}"
 fi
+
+# Copy extra vars file from deepops config dir to k8s config dir for overrides
+cp -v "${deepops_config}/kubespray_deepops_vars.yml" "${k8s_config_dir}/group_vars/all/deepops.yml"
 
 CONFIG_FILE=${k8s_config_dir}/hosts.ini python3 kubespray/contrib/inventory_builder/inventory.py ${@}
