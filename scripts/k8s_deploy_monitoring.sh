52d0368ecac768cb4cb15a9f79c3d01eba788ae4
Douglas Holt <dholt@nvidia.com>
2019-04-03 15:26:15 +0000

    Clean up script

--- scripts/k8s_deploy_monitoring.sh
+++ scripts/k8s_deploy_monitoring.sh
@@ -2,35 +2,32 @@
 
 HELM_COREOS_CHART_REPO="${HELM_COREOS_CHART_REPO:-https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/}"
 
-# Get IP of first master
-master_ip=$(kubectl get nodes -l node-role.kubernetes.io/master= --no-headers -o custom-columns=IP:.status.addresses.*.address | cut -f1 -d, | head -1)
-
-type helm >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-    ./scripts/install_helm.sh
-fi
-
-# Add repo for prometheus charts
-helm repo list | grep coreos >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-    helm repo add coreos "${HELM_COREOS_CHART_REPO}"
-fi
-
 # Determine DeepOps config dir
 config_dir="$(pwd)/config.example"
 if [ "${DEEPOPS_CONFIG_DIR}" ]; then
     config_dir="${DEEPOPS_CONFIG_DIR}"
 fi
 
+# Get IP of first master
+master_ip=$(kubectl get nodes -l node-role.kubernetes.io/master= --no-headers -o custom-columns=IP:.status.addresses.*.address | cut -f1 -d, | head -1)
+
+# Install/initialize Helm client
+if ! type helm >/dev/null 2>&1 ; then
+    ./scripts/install_helm.sh
+fi
+
+# Add repo for Prometheus charts
+if ! helm repo list | grep coreos >/dev/null 2>&1 ; then
+    helm repo add coreos "${HELM_COREOS_CHART_REPO}"
+fi
+
 # Install Prometheus Operator
-helm status prometheus-operator >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
+if ! helm status prometheus-operator >/dev/null 2>&1 ; then
     helm install coreos/prometheus-operator --name prometheus-operator --namespace monitoring --values ${config_dir}/helm/prometheus-operator.yml
 fi
 
 # Create GPU Dashboard config map
-kubectl -n monitoring get configmap kube-prometheus-grafana-gpu >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
+if ! kubectl -n monitoring get configmap kube-prometheus-grafana-gpu >/dev/null 2>&1 ; then
     kubectl create configmap kube-prometheus-grafana-gpu --from-file=${config_dir}/gpu-dashboard.json -n monitoring
 fi
 
@@ -38,9 +35,9 @@ fi
 if ! helm status nginx-ingress >/dev/null 2>&1; then
 	helm install --name nginx-ingress --values "${config_dir}/helm/ingress.yml" stable/nginx-ingress
 fi
+
 # Deploy Monitoring stack
-helm status kube-prometheus >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
+if ! helm status kube-prometheus >/dev/null 2>&1 ; then
     helm install coreos/kube-prometheus --name kube-prometheus --namespace monitoring --values ${config_dir}/helm/kube-prometheus.yml \
         --set alertmanager.ingress.hosts[0]=alertmanager-$(echo ${master_ip} | tr '.' '-').nip.io \
         --set prometheus.ingress.hosts[0]=prometheus-$(echo ${master_ip} | tr '.' '-').nip.io \
@@ -52,8 +49,8 @@ for node in $(kubectl get node --no-headers -o custom-columns=NAME:.metadata.nam
     kubectl label nodes ${node} hardware-type=NVIDIAGPU --overwrite >/dev/null
 done
 
-kubectl -n monitoring get pod -l app=dcgm-exporter 2>&1 | grep "No resources found." >/dev/null 2>&1
-if [ $? -eq 0 ] ; then
+# Deploy DCGM node exporter
+if ! kubectl -n monitoring get pod -l app=dcgm-exporter 2>&1 | grep "No resources found." >/dev/null 2>&1 ; then
     kubectl create -f services/dcgm-exporter.yml
 fi
 
