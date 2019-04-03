1e24451193f782e297d61338d36e285809d0f654
Douglas Holt <dholt@nvidia.com>
2019-04-03 15:26:15 +0000

    Clean up script

--- scripts/k8s_deploy_monitoring.sh
+++ scripts/k8s_deploy_monitoring.sh
@@ -18,11 +18,12 @@ fi
 # Get IP of first master
 master_ip=$(kubectl get nodes -l node-role.kubernetes.io/master= --no-headers -o custom-columns=IP:.status.addresses.*.address | cut -f1 -d, | head -1)
 
-type helm >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
+# Install/initialize Helm client
+if ! type helm >/dev/null 2>&1 ; then
     ./scripts/install_helm.sh
 fi
 
+<<<<<<< HEAD
 case "$1" in
     delete)
         helm del --purge prometheus-operator
