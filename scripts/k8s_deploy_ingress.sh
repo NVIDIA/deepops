caef0c4eaee1c55dae60b50cd2005fea6ef41869
Douglas Holt <dholt@nvidia.com>
2019-04-05 20:18:10 +0000

    split ingress and load balancer scripts, re-add nodeport to monitoring config

--- scripts/k8s_deploy_ingress.sh
+++ scripts/k8s_deploy_ingress.sh
@@ -5,27 +5,28 @@ set -x
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/.."
 
-# Allow overriding the app name with an env var
-app_name="${NGINX_INGRESS_APP_NAME:-nginx-ingress}"
-
 # Allow overriding config dir to look in
 config_dir=${DEEPOPS_CONFIG_DIR:-"${ROOT_DIR}/config"}
-
 if [ ! -d "${config_dir}" ]; then
 	echo "Can't find configuration in ${config_dir}"
 	echo "Please set DEEPOPS_CONFIG_DIR env variable to point to config location"
 	exit 1
 fi
 
-kubectl version
-if [ $? -ne 0 ] ; then
-    echo "Unable to talk to Kubernetes API"
-    exit 1
-fi
-
 # Set up the ingress controller
-if ! helm status "${app_name}" >/dev/null 2>&1; then
-	helm install --name "${app_name}" --values "${config_dir}/helm/ingress.yml" stable/nginx-ingress
+if ! helm status nginx-ingress >/dev/null 2>&1; then
+	helm install --name nginx-ingress --values "${config_dir}/helm/ingress.yml" stable/nginx-ingress
 fi
 
-kubectl wait --for=condition=Ready -l "app=${app_name},component=controller" --timeout=90s pod
+for i in {1..10}; do
+	echo "Check for ingress online... check ${i}"
+	if kubectl get pods -l app=nginx-ingress,component=controller --no-headers | grep -i running;
+	then
+		break
+	else
+		sleep 30
+	fi
+done
+
+# Show services
+kubectl get services
