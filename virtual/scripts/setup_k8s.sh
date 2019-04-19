39bee42ec9d61dbf74d0912900c9251924f49eff
Adam DeConinck <adeconinck@nvidia.com>
2019-04-19 21:04:46 +0000

    remove ip of test mirror

--- virtual/scripts/setup_k8s.sh
+++ virtual/scripts/setup_k8s.sh
@@ -17,8 +17,14 @@ cd "${ROOT_DIR}" || exit 1
 K8S_CONFIG_DIR="${VIRT_DIR}/k8s-config" "${ROOT_DIR}/scripts/k8s_inventory.sh" 10.0.0.2 10.0.0.11
 cp "${VIRT_DIR}/k8s_hosts.ini" "${VIRT_DIR}/k8s-config/hosts.ini"
 
+DEEPOPS_OFFLINE="${DEEPOPS_OFFLINE:-0}"
+k8s_cluster_extra_args=""
+if [ "${DEEPOPS_OFFLINE}" -ne 0 ]; then
+	k8s_cluster_extra_args="-e "@${VIRT_DIR}/config/offline_repo_vars.yml""
+fi
+
 # Deploy the K8s cluster
-ansible-playbook -i "${VIRT_DIR}/k8s-config/hosts.ini" -b "${ROOT_DIR}/playbooks/k8s-cluster.yml"
+ansible-playbook -b -i "${VIRT_DIR}/k8s-config/hosts.ini" ${k8s_cluster_extra_args} "${ROOT_DIR}/playbooks/k8s-cluster.yml"
 
 # Source K8s environment for interacting with the cluster
 # shellcheck disable=SC1091 disable=SC1090
