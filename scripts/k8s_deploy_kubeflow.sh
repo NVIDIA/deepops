b8f7983a9a341ba18f07ffc2e48a8acf66d9cefc
Michael Balint <mbalint@nvidia.com>
2019-04-26 12:49:04 -0400

    Updated to latest version of kubeflow (v0.5.0).

--- scripts/k8s_deploy_kubeflow.sh
+++ scripts/k8s_deploy_kubeflow.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-export KS_VER=0.13.1
-export KS_PKG=ks_${KS_VER}_linux_amd64
-export KS_INSTALL_DIR=/usr/local/bin
-
-export KUBEFLOW_TAG=v0.4.1
+export KUBEFLOW_TAG=v0.5.0
 export KFAPP=kubeflow
 export KUBEFLOW_SRC=/opt/kubeflow
 
-KSONNET_URL="${KSONNET_URL:-https://github.com/ksonnet/ksonnet/releases/download/v${KS_VER}/${KS_PKG}.tar.gz}"
-KUBEFLOW_URL="${KUBEFLOW_URL:-https://raw.githubusercontent.com/kubeflow/kubeflow/${KUBEFLOW_TAG}/scripts/download.sh}"
+KUBEFLOW_URL="${KUBEFLOW_URL:-https://github.com/kubeflow/kubeflow/releases/download/v0.5.0/kfctl_${KUBEFLOW_TAG}_linux.tar.gz}"
 
 ###
 
@@ -42,40 +37,29 @@ if [ $? -eq 0 ] ; then
     exit 1
 fi
 
-# Ksonnet
-wget -O /tmp/${KS_PKG}.tar.gz "${KSONNET_URL}" \
-      --no-check-certificate
-mkdir -p ${KS_INSTALL_DIR}
-tempd=$(mktemp -d)
-tar -xvf /tmp/${KS_PKG}.tar.gz -C ${tempd}
-sudo mv ${tempd}/${KS_PKG}/ks ${KS_INSTALL_DIR}
-rm -rf ${tempd} /tmp/${KS_PKG}.tar.gz
-
 # Kubeflow
 if [ ! -d ${KUBEFLOW_SRC} ] ; then
+    echo ${KUBEFLOW_URL}
     tempd=$(mktemp -d)
     cd ${tempd}
-    curl "${KUBEFLOW_URL}" | bash
+    wget ${KUBEFLOW_URL}
+    tar -xvf "kfctl_${KUBEFLOW_TAG}_linux.tar.gz"
     cd -
     sudo mv ${tempd} ${KUBEFLOW_SRC}
 fi
 
 pushd ${HOME}
-${KUBEFLOW_SRC}/scripts/kfctl.sh init ${KFAPP} --platform none
+${KUBEFLOW_SRC}/kfctl init ${KFAPP}
 cd ${KFAPP}
-${KUBEFLOW_SRC}/scripts/kfctl.sh generate k8s
-pushd ks_app
-ks param set jupyter serviceType NodePort
-popd
-${KUBEFLOW_SRC}/scripts/kfctl.sh apply k8s
-popd
+${KUBEFLOW_SRC}/kfctl generate all
+${KUBEFLOW_SRC}/kfctl apply all
 
-jhip=$(kubectl get nodes --no-headers -o custom-columns=:.status.addresses.*.address -l node-role.kubernetes.io/master= | cut -f1 -d, | head -1)
-jhnp=$(kubectl -n kubeflow get svc jupyter-lb --no-headers -o custom-columns=:.spec.ports.*.nodePort)
+kfip=$(kubectl get nodes --no-headers -o custom-columns=:.status.addresses.*.address -l node-role.kubernetes.io/master= | cut -f1 -d, | head -1)
+kfnp=$(kubectl -n kubeflow get svc ambassador --no-headers -o custom-columns=:.spec.ports.*.nodePort)
 
 echo
 echo "Kubeflow app installed to: ${HOME}/${KFAPP}"
-echo "To remove, run: cd ${HOME}/${KFAPP} && ${KUBEFLOW_SRC}/scripts/kfctl.sh delete k8s"
+echo "To remove, run: cd ${HOME}/${KFAPP} && ${KUBEFLOW_SRC}/kfctl delete all --delete_storage"
 echo
-echo "JupyterHub: http://${jhip}:${jhnp}"
+echo "Kubeflow Ambassador: http://${kfip}:${kfnp}"
 echo
