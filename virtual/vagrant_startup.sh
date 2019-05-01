73dcc6e131116bae1c09c4cf3073dd6e694cf563
Adam DeConinck <adeconinck@nvidia.com>
2019-05-01 14:45:24 -0700

    fix os detection in shell scripts

--- virtual/vagrant_startup.sh
+++ virtual/vagrant_startup.sh
@@ -9,19 +9,19 @@ VIRT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Install Vagrant and Dependencies
 #####################################
 
-case "$ID_LIKE" in
-  rhel*)
+case "$ID" in
+  rhel*|centos*)
     # Install Vagrant & Dependencies for RHEL Systems
 
     export YUM_DEPENDENCIES="centos-release-qemu-ev qem-kvm-ev qemu-kvm libvirt virt-install \
       bridge-utils libvirt-devel libxslt-devel libxml2-devel libguestfs-tools-c sshpass qemu-kvm libvirt-bin \
       libvirt-dev bridge-utils libguestfs-tools qemu virt-manager firewalld OVMF"
 
-    if ! (yum grouplist installed | grep "Development Tools" && rpm -q $YUM_DEPENDENCIES) >/dev/null 2>&1; then
+    if ! (yum grouplist installed | grep "Development Tools" && rpm -q "$YUM_DEPENDENCIES") >/dev/null 2>&1; then
       echo "Installing yum dependencies..."
 
       sudo yum group install -y "Development Tools"
-      sudo yum install -y $YUM_DEPENDENCIES
+      sudo yum install -y "$YUM_DEPENDENCIES"
     fi
 
     # Optional set up networking for Vagrant VMs. Uncomment and adjust if needed
@@ -63,20 +63,20 @@ case "$ID_LIKE" in
     # End Install Vagrant & Dependencies for RHEL Systems
     ;;
 
-  debian*)
+  ubuntu*)
     # Install Vagrant & Dependencies for Debian Systems
 
     export APT_DEPENDENCIES="build-essential sshpass qemu-kvm libvirt-bin libvirt-dev bridge-utils \
       libguestfs-tools qemu ovmf virt-manager firewalld"
 
-    if ! (dpkg -s $APT_DEPENDENCIES) >/dev/null 2>&1; then
+    if ! (dpkg -s "$APT_DEPENDENCIES") >/dev/null 2>&1; then
       echo "Installing apt dependencies..."
 
       # Update apt
       sudo apt update -y
 
       # Install build-essential tools
-      sudo apt install -y $APT_DEPENDENCIES
+      sudo apt install -y "$APT_DEPENDENCIES"
     fi
 
     # Ensure we have permissions to manage VMs
