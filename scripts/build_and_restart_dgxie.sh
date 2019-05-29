4b6a3fe0206e20d4f12030fc3418c2e0f5a8f2a9
Adam Tetelman <supertetelman@gmail.com>
2019-05-29 05:24:08 -0700

    Move dgxie scripts/configuration files into scripts/config directories

---
+++ scripts/build_and_restart_dgxie.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -xe
+
+
+compose_file=config/containers/dgxie/docker-compose.yml
+compose_directory=containers/dgxie/
+compose_cmd="docker-compose --project-directory ${compose_directory} -f ${compose_file}"
+
+
+function tear_down() {
+    ${compose_cmd} down
+}
+
+function build() {
+    ${compose_cmd} build
+}
+
+function bring_up() {
+    ${compose_cmd} up -d
+}
+
+
+tear_down
+build
+bring_up
