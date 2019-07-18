73a8ee400bf348958b0037515dc7732967f40a72
Douglas Holt <dholt@nvidia.com>
2019-07-18 23:05:45 +0000

    update for v2.0

--- containers/setup/doit.sh
+++ containers/setup/doit.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ENROOT_BIN=/usr/local/bin/enroot
+
 export ENROOT_RUNTIME_PATH=${ENROOT_RUNTIME_PATH:-/tmp/enroot/runtime}
 export ENROOT_CACHE_PATH=${ENROOT_CACHE_PATH:-/tmp/enroot/cache}
 export ENROOT_DATA_PATH=${ENROOT_DATA_PATH:-/tmp/enroot/data}
@@ -11,16 +13,16 @@ mkdir -p ${ENROOT_RUNTIME_PATH} ${ENROOT_CACHE_PATH} ${ENROOT_DATA_PATH} ${ENROO
 rm -f ubuntu.sqsh deepops.sqsh deepops.run
 
 # import docker image
-enroot import docker://ubuntu
+${ENROOT_BIN} import docker://ubuntu
 
 # create enroot image
-enroot create ubuntu.sqsh
+${ENROOT_BIN} create ubuntu.sqsh
 
 # start and build container
-enroot batch ./build
+${ENROOT_BIN} batch ./build
 
 # export container as image
-enroot export -o deepops.sqsh ubuntu
+${ENROOT_BIN} export -o deepops.sqsh ubuntu
 
 # bundle image as standalone binary
-enroot bundle deepops.sqsh
+${ENROOT_BIN} bundle deepops.sqsh
