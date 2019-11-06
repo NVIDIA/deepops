23e39f973aafd2a57339d281bb3282f4d452d6f4
Adam DeConinck <adeconinck@nvidia.com>
2019-11-06 13:03:15 -0800

    add PyYAML requirement

--- src/python/setup.py
+++ src/python/setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'PyYAML', ]
 
 setup_requirements = ['pytest-runner', ]
 
