baf90fae1a8059d1c8386bfc8a4e329dfbfc9a7c
Adam DeConinck <adeconinck@nvidia.com>
2019-11-06 13:03:40 -0800

    blackify setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -5,50 +5,53 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'PyYAML', ]
+requirements = [
+    "Click>=7.0",
+    "PyYAML",
+]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="DeepOps Devs",
-    author_email='deepops@nvidia.com',
-    python_requires='>=3',
+    author_email="deepops@nvidia.com",
+    python_requires=">=3",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="DeepOps CLI Installer",
-    entry_points={
-        'console_scripts': [
-            'deepops=deepops.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["deepops=deepops.cli:main",],},
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='deepops',
-    name='deepops',
-    packages=find_packages(include=['deepops', 'deepops.*']),
+    keywords="deepops",
+    name="deepops",
+    packages=find_packages(include=["deepops", "deepops.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/nvidia/deepops',
-    version='0.1.0',
+    url="https://github.com/nvidia/deepops",
+    version="0.1.0",
     zip_safe=False,
 )
