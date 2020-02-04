a339e1d1bc5709a86b790d821a669bc33cbe8845
Adam Tetelman <supertetelman@gmail.com>
2020-02-04 14:57:21 -0800

    Support multiple Kubeflow instances in Jenkins

--- .jenkins-scripts/test-kubeflow.sh
+++ .jenkins-scripts/test-kubeflow.sh
@@ -5,5 +5,8 @@ source .jenkins-scripts/jenkins-common.sh
 # Ensure working directory is root
 cd "${ROOT_DIR}"
 
+export KF_DIR=${ROOT_DIR}/config/kubeflow
+export KFCTL=${ROOT_DIR}/config/kfctl
+
 # Deploy Kubflow, fail if it takes longer than 15 minutes
 timeout 900 ./scripts/k8s_deploy_kubeflow.sh
