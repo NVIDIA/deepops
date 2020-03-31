35eb9f1bd59252d5e071ccab2b1d92aaea084f19
Adam Tetelman <supertetelman@gmail.com>
2020-03-31 15:11:38 -0700

    Fail Jenkins if vagrant up fails

--- .jenkins-scripts/vagrant-startup.sh
+++ .jenkins-scripts/vagrant-startup.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -ex
 source .jenkins-scripts/jenkins-common.sh
 
 cd virtual || exit 1
-bash ./vagrant_startup.sh
-cat config/inventory
+bash ./vagrant_startup.sh # If this fails the entire test should halt
+cat inventory* # We can't look at config/inventory because that is created after this step
 cat Vagrantfile
