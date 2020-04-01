5431274d901dcf2ec8fbb3ab4f2df103bbc008ae
Adam Tetelman <supertetelman@gmail.com>
2020-03-31 21:54:26 -0700

    Fix inventoryfile name in Jenkins debug

--- .jenkins-scripts/vagrant-startup.sh
+++ .jenkins-scripts/vagrant-startup.sh
@@ -4,5 +4,5 @@ source .jenkins-scripts/jenkins-common.sh
 
 cd virtual || exit 1
 bash ./vagrant_startup.sh # If this fails the entire test should halt
-cat inventory* # We can't look at config/inventory because that is created after this step
+cat virtual_inventory* # We can't look at config/inventory because that is created after this step
 cat Vagrantfile
