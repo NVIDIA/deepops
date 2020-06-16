cc3957bd452aaa3b42d03322ba17d9dbda416c83
Issam SAID <isaid@nvidia.com>
2020-06-16 18:36:14 +0400

    remove unecessary templates

--- roles/easy-build/templates/z01_eb.csh
+++
@@ -1,5 +0,0 @@
-setenv EASYBUILD_PREFIX {{ sm_prefix }}
-setenv EASYBUILD_MODULES_TOOL Lmod
-module purge
-unset $(env | grep EBROOT | awk -F'=' '{print $1}')
-module load EasyBuild
--- roles/easy-build/templates/z01_eb.sh
+++
@@ -1,5 +0,0 @@
-export EASYBUILD_PREFIX={{ sm_prefix }}
-export EASYBUILD_MODULES_TOOL=Lmod
-module purge
-unset $(env | grep EBROOT | awk -F'=' '{print $1}')
-module load EasyBuild
