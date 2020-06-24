1e9ebfc1d1a554d58f5629f33a615e1d9b337644
Adam Tetelman <supertetelman@gmail.com>
2020-06-24 20:29:29 +0000

    Fix minor timing bug in ceph-poll script

--- scripts/ceph_poll.sh
+++ scripts/ceph_poll.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# See https://rook.io/docs/rook/v1.1/ceph-quickstart.html
 echo "Beginning to poll for Ceph and Rook setup completion."
 echo "This may throw several errors and take up to 10 minutes. This behavior is expected."
-
-rook_tools_pod=$(kubectl -n rook-ceph get pod -l app=rook-ceph-tools -o name | cut -d \/ -f2 | sed -e 's/\\r$//g')
+echo "The script will polling when Ceph setup is completed and in a healthy state".
 
 while true; do
+    rook_tools_pod=$(kubectl -n rook-ceph get pod -l app=rook-ceph-tools -o name | cut -d \/ -f2 | sed -e 's/\\r$//g')
     kubectl -n rook-ceph exec -ti $rook_tools_pod ceph status # Run once to print output
     kubectl -n rook-ceph exec -ti $rook_tools_pod ceph status | grep "mds: cephfs" | grep "up:active" | grep "standby-replay" # Run again to check for completion
     if [ "${?}" == "0" ]; then
