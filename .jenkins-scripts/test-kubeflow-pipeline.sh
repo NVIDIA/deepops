46e9b75fa1ac04649b3deae184102f34993a7fa0
Adam Tetelman <supertetelman@gmail.com>
2020-07-31 00:43:50 +0000

    Add a quik test to actually launch a container via Kubeflow pipelines

---
+++ .jenkins-scripts/test-kubeflow-pipeline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+source .jenkins-scripts/jenkins-common.sh
+
+# Ensure working directory is root
+cd "${ROOT_DIR}"
+
+# Install the optional kfp package
+sudo pip3 install kfp
+
+# Wait for the kubeflow pipeline service to be ready, and then wait another 30 seconds for other random Kubeflow initialization
+# Don't wait for katib or a few other things that take longer to initialize
+export KUBEFLOW_DEPLOYMENTS="profiles-deployment centraldashboard ml-pipeline minio mysql metadata-db"
+./scripts/k8s_deploy_kubeflow.sh -w
+sleep 30
+
+# Run the Kubeflow pipeline test, this will build a pipeline that launches an NGC container
+timeout 600 python3 .jenkins-scripts/test-kubeflow-pipeline.py
