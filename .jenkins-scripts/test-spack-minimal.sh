fc546230d42e302e12adb5e80fea9ac03c8b8d23
Adam DeConinck <adeconinck@nvidia.com>
2020-08-04 16:01:54 +0000

    Add debugging to the spack script

--- .jenkins-scripts/test-spack-minimal.sh
+++ .jenkins-scripts/test-spack-minimal.sh
@@ -2,7 +2,25 @@
 set -e
 source .jenkins-scripts/jenkins-common.sh
 
+echo
+echo "Current environment"
+echo
+env
+
+echo
+echo "Which Ansible are we using?"
+echo
+which ansible-playbook
+
+echo 
+echo "Show Ansible details"
+echo
+ansible-playbook --version
+
 # Install Spack, but do not install any modules
+echo
+echo "Install Spack"
+echo
 ansible-playbook -i virtual/config/inventory playbooks/spack-modules.sh
 
 # After install, we expect spack to be in our PATH
