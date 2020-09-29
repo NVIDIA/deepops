256f90bdf784210e134eab8ca8de89c6cdfe3eda
Douglas Holt <dholt@nvidia.com>
2020-09-29 15:15:01 -0700

    Fix incorrect paths in k8s deploy ingress script

--- scripts/k8s/deploy_ingress.sh
+++ scripts/k8s/deploy_ingress.sh
@@ -3,10 +3,10 @@ set -x
 
 # Get absolute path for script and root
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_DIR="${SCRIPT_DIR}/.."
+ROOT_DIR="${SCRIPT_DIR}/../.."
 CHART_VERSION="1.22.1"
 
-./scripts/install_helm.sh
+./scripts/k8s/install_helm.sh
 
 # Allow overriding the app name with an env var
 app_name="${NGINX_INGRESS_APP_NAME:-nginx-ingress}"
