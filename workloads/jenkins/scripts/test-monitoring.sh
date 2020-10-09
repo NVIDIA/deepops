463622d4a5e708caf39d2386b5dcaba5d4038401
Adam Tetelman <supertetelman@gmail.com>
2020-10-09 20:24:46 +0000

    Update monitoring test to fail on timeout and on 404

--- workloads/jenkins/scripts/test-monitoring.sh
+++ workloads/jenkins/scripts/test-monitoring.sh
@@ -13,13 +13,23 @@ source ./scripts/k8s/deploy_monitoring.sh
 timeout=600
 time=0
 while [ ${time} -lt ${timeout} ]; do
-  curl -s --raw -L "${prometheus_url}" && \
-    curl -s --raw -L "${grafana_url}" && \
-    curl -s --raw -L "${alertmanager_url}"  && \
-    echo "Monitoring URLs are all responding" && break
+  curl -s --raw -L "${prometheus_url}"     | grep Prometheus && \
+    curl -s --raw -L "${grafana_url}"      | grep Grafana && \
+    curl -s --raw -L "${alertmanager_url}" | grep Alertmanager && \
+    echo "Monitoring URLs are all responding" && \
+    pass=true && break
   let time=$time+15
   sleep 15
 done
+
+# Fail if timed out
+if [ "${pass}" != "true" ]; then
+  echo "Timed out getting monitoring responses"
+  curl -s --raw -L "${prometheus_url}"
+  curl -s --raw -L "${grafana_url}"
+  curl -s --raw -L "${alertmanager_url}"
+  exit 1
+fi
 
 # Delete Monitoring
 source ./scripts/k8s/deploy_monitoring.sh -d && exit 0
