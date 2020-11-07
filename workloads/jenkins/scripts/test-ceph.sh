8e5912bae6cfbbb4905ba8393e656fa203bd78dc
Adam Tetelman <supertetelman@gmail.com>
2020-11-07 00:36:44 +0000

    Add rook option to install as non-default SC, use this in tests.

--- workloads/jenkins/scripts/test-ceph.sh
+++ workloads/jenkins/scripts/test-ceph.sh
@@ -6,7 +6,7 @@ source workloads/jenkins/scripts/jenkins-common.sh
 cd "${ROOT_DIR}" || exit 1
 
 # Deploy rook, fail if it takes longer than 5 minutes
-timeout 300 ./scripts/k8s/deploy_rook.sh
+timeout 300 ./scripts/k8s/deploy_rook.sh -x
 
 # Poll for completion, fail if it takes longer than 30 minutes
 timeout 1800 ./scripts/k8s/deploy_rook.sh -w
