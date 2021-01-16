1c0748ac8decdd6b855880f05a1a66ce7ba7f97f
Adam Tetelman <supertetelman@gmail.com>
2021-01-16 01:45:29 +0000

    Update the run gpu test to use a container that still exists

--- workloads/jenkins/scripts/run-gpu-job.sh
+++ workloads/jenkins/scripts/run-gpu-job.sh
@@ -23,7 +23,7 @@ fi
 
 # Occassionally this gpu-test fails and/or hangs. To ease debugging of this we run a describe several seconds into the launch.
 sleep 10 && kubectl describe pods gpu-test &
-timeout 300 kubectl run gpu-test --rm -t -i --restart=Never --image=nvidia/cuda --limits=nvidia.com/gpu=1 -- nvidia-smi
+timeout 300 kubectl run gpu-test --rm -t -i --restart=Never --image=nvcr.io/nvidia/cuda:10.1-base-ubuntu18.04 --limits=nvidia.com/gpu=1 -- nvidia-smi
 
 # Run multi-GPU test
 if [ ${DEEPOPS_FULL_INSTALL} ]; then
