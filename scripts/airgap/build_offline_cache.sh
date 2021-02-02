78f2e72d2d56c60d4a03e49f69a24c8d30217a79
Fredrik Andersson <fredrik.and@gmail.com>
2021-02-02 08:47:59 +0100

    Update default values for helm charts repositories
    New Location For Stable and Incubator Charts
    https://helm.sh/blog/new-location-stable-incubator-charts/
    
    Add default value for JupyterHub helm charts
    https://jupyterhub.github.io/helm-chart/
    
    Signed-off-by: Fredrik Andersson <fredrik.and@gmail.com>

--- scripts/airgap/build_offline_cache.sh
+++ scripts/airgap/build_offline_cache.sh
@@ -54,8 +54,9 @@ get_all_helm_tgzs() {
 echo "Mirroring Helm charts locally"
 HELM_DEST_DIR="${HELM_DEST_DIR:-${DEST_DIR}/helm}"
 
-HELM_STABLE_CHARTS_URL="${HELM_STABLE_CHARTS_URL:-https://kubernetes-charts.storage.googleapis.com}"
+HELM_STABLE_CHARTS_URL="${HELM_STABLE_CHARTS_URL:-https://charts.helm.sh/stable}"
 HELM_ROOK_CHARTS_URL="${HELM_ROOK_CHARTS_URL:-https://charts.rook.io/master}"
+HELM_JUPYTER_CHARTS_URL="${HELM_JUPYTER_CHARTS_URL:-https://jupyterhub.github.io/helm-chart}"
 
 mkdir -p "${HELM_DEST_DIR}/mirror"
 pushd "${HELM_DEST_DIR}"
