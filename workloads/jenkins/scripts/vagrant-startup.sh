f30f6be91529872445334db37a60a3f758350fd7
Adam Tetelman <supertetelman@gmail.com>
2021-03-25 14:26:07 -0700

    Add better/concistent vagrant cleanup to jenkins vagrant-startup script

--- workloads/jenkins/scripts/vagrant-startup.sh
+++ workloads/jenkins/scripts/vagrant-startup.sh
@@ -3,6 +3,7 @@ set -ex
 source workloads/jenkins/scripts/jenkins-common.sh
 
 cd virtual || exit 1
+bash ./vagrant_shutdown.sh || true # Some previous VMs may not have been cleaned; this may fail if the environment is clean; so we proceed regardless
 bash ./vagrant_startup.sh # If this fails the entire test should halt
 cat virtual_inventory* # We can't look at config/inventory because that is created after this step
 cat Vagrantfile
