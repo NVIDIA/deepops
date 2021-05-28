8288cdc68c602ca322ad47f2a0a12f1307c8f1a6
Adam Tetelman <supertetelman@gmail.com>
2021-05-28 18:02:13 +0000

    Cleanup extra lines/whitespace in script

--- workloads/jenkins/scripts/test-dcgm-metrics.sh
+++ workloads/jenkins/scripts/test-dcgm-metrics.sh
@@ -6,12 +6,10 @@ source workloads/jenkins/scripts/jenkins-common.sh
 # Ensure working directory is root
 cd "${ROOT_DIR}"
 
-
 DCGM_EXPORTER_PORT=9400
 
 # Run DCGM metric checks against all nodes in the group passed in (kube-node or slurm-node)
 group="${1}"
-
 
 # DCGM-exporter takes some time to initialize after it has started up
 # Before checking specific metrics we poll for any DCGM metrics to return
@@ -22,14 +20,14 @@ set +e # This polling is expected to fail, so remove the -e flag for the loop
 while [ ${time} -lt ${timeout} ]; do
 # Collect metrics from all nodes for debug
   ansible ${group} -vv -m raw \
+    -e "@virtual/vars_files/virt_k8s.yml" \
+    ${ansible_extra_args} \
+    -b -i "virtual/config/inventory" \
     -a "curl http://127.0.0.1:${DCGM_EXPORTER_PORT}/metrics | grep DCGM" && break
     let time=$time+5
     sleep 5
 done
 set -e
-#    -e "@virtual/vars_files/virt_k8s.yml" \
-#    ${ansible_extra_args} \
-#    -b -i "virtual/config/inventory" \
 
 # Collect metrics from all nodes for debug
 ansible ${group} -vv -m raw \
