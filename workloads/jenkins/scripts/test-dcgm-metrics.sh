c924c8b3bf076ea5de46e9bb70192256b4eb3838
Adam Tetelman <supertetelman@gmail.com>
2021-05-28 05:25:07 +0000

    Update new DCGM test to run against slurm-node in Slurm deployments

--- workloads/jenkins/scripts/test-dcgm-metrics.sh
+++ workloads/jenkins/scripts/test-dcgm-metrics.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Verify that the DCGM-exporter is returning all metrics configured in the Grafana dashboard, for each node
 set -ex
 source workloads/jenkins/scripts/jenkins-common.sh
 
@@ -8,13 +9,15 @@ cd "${ROOT_DIR}"
 
 DCGM_EXPORTER_PORT=9400
 
+# Run DCGM metric checks against all nodes in the group passed in (kube-node or slurm-node)
+group="${1}"
 
 # Get an up-to-date list of all DCGM metrics included in the default dashboard, with some awk magic
 dcgm_metrics=$(grep DCGM ${ROOT_DIR}/src/dashboards/gpu-dashboard.json   | awk -F\{ '{print $1}' | awk -F"DCGM" '{print "DCGM"$2}' | sort | uniq)
 
 # Verify all DCGM metrics from the default dashboard are being returned by the DCGM-exporter
 for metric in ${dcgm_metrics}; do
-  ansible kube-node -vv -m raw \
+  ansible ${group} -vv -m raw \
     -b -i "virtual/config/inventory" \
     -e "@virtual/vars_files/virt_k8s.yml" \
     ${ansible_extra_args} \
--- workloads/jenkins/scripts/test-monitoring.sh
+++ workloads/jenkins/scripts/test-monitoring.sh
@@ -64,7 +64,7 @@ fi
 
 # Get some debug for Pods that did/didn't come up and verify DCGM metrics
 kubectl get all -n monitoring
-bash -x ./workloads/jenkins/scripts/test-dcgm-metrics.sh
+bash -x ./workloads/jenkins/scripts/test-dcgm-metrics.sh kube-node
 
 # Delete Monitoring
 ./scripts/k8s/deploy_monitoring.sh -d && exit 0
