dd22cfbbdb6d66b16efd81f6dab0b7020a4d1424
Adam Tetelman <supertetelman@gmail.com>
2021-09-28 13:06:53 -0700

    Make k8s debug script work outside DeepOps and improve vGPU/GPU Operator debug

--- scripts/k8s/debug.sh
+++ scripts/k8s/debug.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+# This is a basic debug script for Kubernetes clusters
+# Please use this script to collect a log bundle when opening a support request or asking for debug assistance
+
+# Ideally this is run out of the DeepOps repo used to deploy the cluster
+# However, this script will also work best-effort for any K8s cluster, DeepOps or otherwise
+# Requirements for this script are a working "kubectl" and ideally a working "helm"
+# Optionally, a working "ansible" with a config/inventory file that has kubernetes node defined in a kube-node group
+
 timestamp=$(date +%s)
 logdir=config/log_${timestamp}
-mkdir ${logdir}
+mkdir -p ${logdir}
 
-# Provisioner configuration
+# Provisioner configuration (specific to DeepOps deployments)
 cp config/inventory ${logdir}
 git branch > ${logdir}/git-branch.log
 git status > ${logdir}/git-status.log
@@ -12,23 +20,36 @@ git log --pretty=oneline | head -n 20 > ${logdir}/git-log.log
 
 # GPU configuration
 ansible kube-node -ba "nvidia-smi" -vv > ${logdir}/nvidia-smi.log
+ansible kube-node -ba "cat /etc/nvidia/gridd.conf" -vv > ${logdir}/vgpu-gridd.conf.log
 
 # Docker configuration
 ansible kube-node -ba "docker info" -vv > ${logdir}/docker-info.log
 ansible kube-node -ba "cat /etc/docker/daemon.json" -vv > ${logdir}/docker-daemon.log
 
-# Kubectl
+# Kubectl (Generic for any Kubernetes cluster)
 kubectl get pvc -A > ${logdir}/get-pvc.log
 kubectl get pv -A > ${logdir}/get-pv.log
 kubectl get pods -A > ${logdir}/get-pods.log
 kubectl get daemonset -A > ${logdir}/get-daemons.log
 kubectl get nodes > ${logdir}/get-nodes.log
 kubectl describe nodes > ${logdir}/describe-nodes.log
-for pod in $(kubectl get pods -n kube-system | grep nvidia-device-plugin | awk '{print $1}'); do
-  kubectl -n kube-system logs ${pod} > ${logdir}/get-plugin-logs-${pod}.log
-done
 kubectl get storageclass > ${logdir}/get-storageclass.log
 kubectl get events -A > ${logdir}/get-events.log
+# Kubectl / GPU Operator (Generic for any Kubernetes cluster)
+kubectl get pvc -A > ${logdir}/get-pvc.log
+for pod in $(kubectl get pods -n gpu-operator-resources  | grep nvidia-device-plugin | awk '{print $1}'); do
+  kubectl -n gpu-operator-resources  logs ${pod} > ${logdir}/get-plugin-logs-${pod}.log
+done
+for pod in $(kubectl get pods -n gpu-operator-resources  | grep gpu-feature-discovery | awk '{print $1}'); do
+  kubectl -n gpu-operator-resources  logs ${pod} > ${logdir}/get-plugin-logs-${pod}.log
+done
+for pod in $(kubectl get pods -n gpu-operator-resources  | grep nvidia-operator-validator | awk '{print $1}'); do
+  kubectl -n gpu-operator-resources  logs ${pod} > ${logdir}/get-plugin-logs-${pod}.log
+done
+for pod in $(kubectl get pods -n gpu-operator-resources  | grep driver | awk '{print $1}'); do
+  kubectl -n gpu-operator-resources  logs ${pod} > ${logdir}/get-plugin-logs-${pod}.log
+done
+kubectl describe pods -n gpu-operator-resources > ${logdir}/describe-gpu-operator-resources-pods.log
 
 # Helm
 helm list -aA > ${logdir}/helm-list.log
