32729eca12464e0f0103e0a7ad8e0ddafb3b0bfe
Douglas Holt <dholt@nvidia.com>
2021-10-14 13:56:04 -0700

    try waiting for operator validator pod

--- workloads/jenkins/scripts/run-gpu-job.sh
+++ workloads/jenkins/scripts/run-gpu-job.sh
@@ -11,13 +11,10 @@ chmod 755 "$K8S_CONFIG_DIR/artifacts/kubectl"
 kubectl get nodes
 kubectl describe nodes
 
-# Wait for GPU feature discovery to finish
-if kubectl get pods -n gpu-operator-resources | grep feature-discovery ; then
-    kubectl wait --for=condition=ready --timeout=600s pod -n gpu-operator-resources -l app=gpu-feature-discovery
+# Wait for GPU operator to finish
+if kubectl get pods -n gpu-operator-resources | grep nvidia-operator-validator ; then
+    kubectl wait --for=condition=ready --timeout=600s pod -n gpu-operator-resources -l app=nvidia-operator-validator
 fi
-
-# Wait a couple minutes for the GPU operator to do its thing
-sleep 120
 
 # Verify GPU Feature Discovery was installed and one or more nodes were labeled, run queries and remove new lines/white space/non-gpu node output
 strategy=$(kubectl get node -o=custom-columns=:.metadata.labels.nvidia\\.com/mig\\.strategy | grep -v none | tr -d '\040\011\012\015')
