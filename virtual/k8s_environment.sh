9cdc5931868ec80cb0d2c0e9ef8821192061817d
EricMarcus-ai <ericj.marcus@gmail.com>
2024-06-03 15:57:37 +0200

    Removes virtual and submodules

--- virtual/k8s_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Set up local environment to work with virtual k8s cluster
-
-K8S_CONFIG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/config"
-
-export KUBECONFIG="${K8S_CONFIG_DIR}/artifacts/admin.conf"
-export PATH="${K8S_CONFIG_DIR}/artifacts:${PATH}"
